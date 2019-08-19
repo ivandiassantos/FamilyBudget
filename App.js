56a1c564d215225222ea02a705e87465c9e2822a
Ivan Dias <ivan.dias.santos@gmail.com>
2019-08-18 23:01:04 -0300

    Criação de página de login

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import { createStackNavigator, createSwitchNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import OtherScreen from './src/screens/OtherScreen';
@@ -21,7 +24,26 @@ const AppNavigator = createAppContainer(createSwitchNavigator(
 ));
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
   render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
     return (<AppNavigator />);
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
   }
 }
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,37 @@
-import React, {Component} from 'react';
-import {View, Button, AsyncStorage} from 'react-native';
+import React, { Component } from 'react';
+import { AsyncStorage } from 'react-native';
+import { Container, Content, Form, Item, Label, Input, Button, Text, Icon } from 'native-base';
 
 export default class LoginScreen extends Component {
     static navigationOptions = {
-        title: 'Please sign in',
+        title: 'Family Budget - Login',
     };
 
     render() {
         return (
-            <View>
-                <Button title="Sign in!" onPress={this._signInAsync} />
-            </View>
+            <Container>
+                <Content>
+                    <Form>
+                        <Item floatingLabel>
+                            <Label>Login</Label>
+                            <Input />
+                        </Item>
+                        <Item floatingLabel last>
+                            <Label>Senha</Label>
+                            <Input />
+                        </Item>
+                        <Button full onPress={this._signInAsync}>
+                            <Text>ENTRAR</Text>
+                        </Button>
+                        <Button iconLeft full>
+                            <Text>ENTRAR COM CONTA FACEBOOK</Text>
+                        </Button>
+                        <Button iconLeft full>
+                            <Text>ENTRAR COM CONTA GOOGLE</Text>
+                        </Button>
+                    </Form>
+                </Content>
+            </Container>
         );
     }
 
